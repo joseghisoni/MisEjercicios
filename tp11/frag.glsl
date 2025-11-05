b363d2cca05b59d1c652febc0586c04775285c75
noehsueh <noehsueh@gmail.com>
2025-11-05 17:03:35 -0300

    Add: template para frag shader

--- tp11/frag.glsl
+++ tp11/frag.glsl
@@ -1,5 +1,15 @@
-// Fragment Shader
+// Fragment Shader del cielo
+// En este shader construimos rayos por píxel, pero sólo para mostrar un cielo simple
 precision mediump float;
+varying vec2 vUV; // Coordenadas normalizadas de pantalla (0..1)
+
+// Parámetros de cámara y proyección
+uniform vec2 uResolution;   // tamaño del canvas en píxeles
+uniform float uTanHalfFov;   // tan(fov/2)
+uniform mat3 uCamRot;       // rotación de la cámara (mundo)
+uniform vec3 uCamPos;       // posición de la cámara (mundo)
+
+
 
 void main(){
     vec3 color=vec3(0.0, 0.5, 1.0); // Color azul claro
